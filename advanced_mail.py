be84e7c21bcb57907fc865cce12bcb8e625e637e
Pim Duivestein <pimduif@gmail.com>
2024-01-24 20:29:21 +0100

    commit

--- advanced_mail.py
+++ advanced_mail.py
@@ -3,16 +3,17 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 sender_email = "t7133657@gmail.com"
-receiver_email = ["marijn2huis@gmail.com"]
-password = "-"
-cc = ['marijn2huis@gmail.com', 'pimduif@gmail.com', 't7133657@gmail.com']
+receiver_email = ["t7133657@gmail.com"]
+password = "jwmo yrmh kafo wjns"
+# cc = ['marijn2huis@gmail.com', 'pimduif@gmail.com', 't7133657@gmail.com']
 
 message = MIMEMultipart("alternative")
 message["Subject"] = "Birthday Message"
 message["From"] = sender_email
 message["To"] = ','.join(receiver_email)
-message['Cc'] = ','.join(cc)
+# message['Cc'] = ','.join(cc)
 
+placeholder = 'ronald'
 # Create the plain-text and HTML version of your message
 text = """\
 Hi Big T,
@@ -20,14 +21,32 @@ How are you?
 It's Rory's birthday in 1 week.
 """
 
-html = """\
+html = f"""\
 <html>
   <body>
-    <h1>Hi Big T</h1>
-    <p>How are you?<br>
-        It's Rory's birthday in 1 week. If you need a present, click
-       <a href="https://www.bol.com/nl/nl/p/mankini-borat-string/9200000103544012/">here</a>.
+    <h1>Verjaardag reminder</h1>
+    <p> Het is {placeholder} zijn verjaardag over {placeholder} dagen.
     </p>
+    <div class="table">
+      <h1>Komende verjaardagen</h1>
+        <table class="table-data">
+            <tr>
+                <th>Naam</th>
+                <th>Datum</th>
+                <th>Dagen</th>
+            </tr>
+            <tr>
+                <td>Tijn</td>
+                <td>10 Augustus</td>
+                <td>100</td>
+            </tr>
+            <tr>
+                <td>Kwint</td>
+                <td>11 Augustus</td>
+                <td>20</td>
+            </tr>
+        </table>
+    </div>
   </body>
 </html>
 """
@@ -44,5 +63,5 @@ server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(sender_email, password)
 # sending the mail and terminate the session
-server.sendmail(sender_email, receiver_email+cc, message.as_string())
+server.sendmail(sender_email, receiver_email, message.as_string())
 server.quit()
