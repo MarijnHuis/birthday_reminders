9c6f1e90c15a9360100b84b3c38cc6260c0ea065
Pim Duivestein <pimduif@gmail.com>
2024-02-01 19:41:16 +0100

    batch file

---
+++ birthdays/reminder.bat
@@ -0,0 +1,3 @@
+call C:\Users\Administrator\miniconda3\Scripts\activate.bat
+call conda activate algo_env
+call python C:\Users\Administrator\Desktop\birthday_reminders-main\birthday_reminders-main\birthdays\main.py
